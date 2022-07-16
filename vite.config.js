8dc2aef92480240b872650d6a4e110df1c6d1556
andres <jorquera.ad@gmail.com>
2022-07-16 15:31:35 +0200

    config for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+const isProd = process.env.NODE_ENV === 'production'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  base: isProd ? '/test' : ''
 })
